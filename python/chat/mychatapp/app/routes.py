3252a2872f67ee62fe610db0772d520e4053d3e8
Varuzhan <varuzhangevorgian@gmail.com>
2023-10-08 15:31:04 +0400

    Deleted unecessary files.

--- python/chat/mychatapp/app/routes.py
+++
@@ -1,6 +0,0 @@
-from flask import render_template, request, redirect, url_for, flash
-from app import app, db
-from app.models import User, Message
-
-# Define your routes and views here
-# Implement registration, login, and chat routes
